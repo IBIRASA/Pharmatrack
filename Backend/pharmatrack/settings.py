b1c17834dff2f4c593eda37e2acea3d0194f0baf
IBIRASA <divinebirasaishimwe@gmail.com>
2025-10-07 20:32:52 +0200

    urls

--- Backend/pharmatrack/settings.py
+++ Backend/pharmatrack/settings.py
@@ -128,10 +128,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
-    "https://pharmatrackk.netlify.app/",
+    "https://pharmatrackk.netlify.app",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://pharmatrackk.netlify.app/",
+    "https://pharmatrackk.netlify.app",
 ]
 
