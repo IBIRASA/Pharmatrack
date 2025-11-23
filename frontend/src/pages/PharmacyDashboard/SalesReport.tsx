bbe1f403890f3b1d9a7b1c995dc952fe65eec2c6
IBIRASA <d.ishimwe@alustudent.com>
2025-11-24 01:46:51 +0200

    remove customer duplicate

--- frontend/src/pages/PharmacyDashboard/SalesReport.tsx
+++ frontend/src/pages/PharmacyDashboard/SalesReport.tsx
@@ -286,12 +286,12 @@ export default function SalesReport() {
                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   Transaction ID
                 </th>
-                {/* <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   Customer
                 </th>
                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   Medicine
-                </th> */}
+                </th>
                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   Quantity
                 </th>
